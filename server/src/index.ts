4de3fdb760ac8198c7f5a627ddfd8faa65e525e2
Nnamani John <nnamanijohn123@gmail.com>
2024-09-13 13:45:12 +0100

    chore: add jest for testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,4 +2,6 @@ const sayHello = (name: string): void => {
   console.log(`Hello, ${name}!`);
 };
 
+export const sum = (a: number, b: number) => a + b;
+
 sayHello('World');
---
+++ server/src/tests/index.test.ts
@@ -0,0 +1,7 @@
+import { sum } from '../index';
+
+describe('Example Test', () => {
+  test('should add numbers correctly', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
