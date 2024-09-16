2c9442ae997ea3128e95945fdf695c8af2c0ca40
Nnamani John <nnamanijohn123@gmail.com>
2024-09-16 15:26:38 +0100

    feat: jest unit test for project endpoint

--- server/src/tests/index.test.ts
+++ server/src/tests/index.test.ts
@@ -1,5 +1,16 @@
 import request from 'supertest';
 import { app, server } from '../index';
+import mongoose from 'mongoose';
+import sequelize from '../database';
+
+let projectId: number;
+beforeAll(async () => {
+  const response = await request(app)
+    .post('/projects')
+    .send({ title: 'Test Project', description: 'Description' });
+
+  projectId = response.body.data.id;
+});
 
 describe('GET /', () => {
   it('should return a greeting message', async () => {
@@ -10,6 +21,66 @@ describe('GET /', () => {
   });
 });
 
-afterAll(() => {
+describe('Project Routes', () => {
+  it('GET /projects', async () => {
+    const response = await request(app).get('/projects');
+
+    expect(response.status).toBe(200);
+  });
+
+  it('POST /projects', async () => {
+    const response = await request(app)
+      .post('/projects')
+      .send({ title: 'Test Project', description: 'Description' });
+
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+  });
+
+  it('GET /projects/:id', async () => {
+    const response = await request(app).get(`/projects/${projectId}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('PUT /projects/:id', async () => {
+    const response = await request(app)
+      .put(`/projects/${projectId}`)
+      .send({ title: 'Updated Project', description: 'Updated Description' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+  });
+
+  it('DELETE /projects/:id', async () => {
+    const response = await request(app).delete(`/projects/${projectId}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+  });
+
+  it('GET /projects/:id/tasks', async () => {
+    const response = await request(app).get(`/projects/${projectId}/tasks`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('POST /projects/:id/tasks', async () => {
+    const response = await request(app)
+      .post(`/projects/${projectId}/tasks`)
+      .send({
+        title: 'Test Task',
+        description: 'Task Description',
+        status: 'pending',
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await sequelize.close();
   server.close();
 });
